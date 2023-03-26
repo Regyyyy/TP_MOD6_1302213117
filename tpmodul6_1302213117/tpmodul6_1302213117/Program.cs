6e7fce422143573909ebed9e3c024431a1eaedb7
Regyyyy <regyrahman@gmail.com>
2023-03-26 14:45:00 +0700

    Second Commit : Class SayaTubeVideo
    menambahkan class SayaTubeVideo

--- tpmodul6_1302213117/tpmodul6_1302213117/Program.cs
+++ tpmodul6_1302213117/tpmodul6_1302213117/Program.cs
@@ -2,6 +2,35 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - Regy Renanda Rahman");
+        video.IncreasePlayCount(1);
+        video.PrintVideoDetails();
+    }
+}
+
+class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        Random random = new Random();
+        id = random.Next(10000,100000);
+        this.title = title;
+        playCount = 0;
+    }
+
+    public void IncreasePlayCount(int angka)
+    {
+        playCount += angka;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("ID         : "+id);
+        Console.WriteLine("Title      : "+title);
+        Console.WriteLine("Play Count : "+playCount);
     }
 }
